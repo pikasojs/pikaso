fb023bb72ecca370d6e8fa9e65a83a5bc664da50
Ramin <ramin.mousavy@gmail.com>
2020-11-22 23:32:34 +0300

    fix(omit): fix types

--- src/utils/omit/index.ts
+++ src/utils/omit/index.ts
@@ -1,4 +1,6 @@
-export function omit(state: object, keys: string[]) {
+import type { UnknownObject } from '../../types'
+
+export function omit(state: UnknownObject, keys: string[]) {
   return Object.entries(state).reduce((acc, [key, value]) => {
     if (keys.includes(key)) {
       return acc
