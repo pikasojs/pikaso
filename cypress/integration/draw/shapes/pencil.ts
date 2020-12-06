e5f6e52ec2a1ad254f9aa4d0a41c506a74c78d72
Ramin <ramin.mousavy@gmail.com>
2020-12-06 23:22:59 +0300

    feat(tests): add tests for pencil free drawing

---
+++ cypress/integration/draw/shapes/pencil.ts
@@ -0,0 +1,57 @@
+/// <reference types="cypress" />
+/// <reference path="../../../support/index.d.ts" />
+
+describe('Shapes -> Pencil', () => {
+  beforeEach(() => {
+    cy.reload()
+  })
+
+  it('should free draw a line', () => {
+    cy.getEditor().then(editor => {
+      editor.pencil.draw({
+        stroke: 'red',
+        strokeWidth: 10
+      })
+
+      return cy
+        .get('canvas')
+        .trigger('mousedown', {
+          clientX: 100,
+          clientY: 100
+        })
+        .trigger('mousemove', {
+          clientX: 110,
+          clientY: 110
+        })
+        .trigger('mousemove', {
+          clientX: 100,
+          clientY: 210
+        })
+        .trigger('mousemove', {
+          clientX: 500,
+          clientY: 300
+        })
+        .trigger('mouseup', { force: true })
+        .then(() => {
+          expect(editor.board.getShapes().length).equal(1)
+        })
+    })
+  })
+
+  it('should select the free drawn line', () => {
+    cy.getEditor().then(editor => {
+      editor.pencil.draw({
+        stroke: 'red',
+        strokeWidth: 15
+      })
+
+      cy.draw([100, 100], [500, 500]).then(() => {
+        cy.mouseTrigger('mousedown', [200, 200], {
+          delay: 1000
+        }).then(() => {
+          expect(editor.selection.transformer.attrs.visible).equal(true)
+        })
+      })
+    })
+  })
+})
