47864f56f24118b956f2836862ce4e7aacfaa109
Ramin <ramin.mousavy@gmail.com>
2020-12-11 15:48:33 +0300

    test(filter): test all scenarios of shape filters

---
+++ tests/integration/filter/index.test.ts
@@ -0,0 +1,166 @@
+import Konva from 'konva'
+
+import { createEditor } from '../../../jest/utils/create-editor'
+
+describe('Filter', () => {
+  const shapeConfig = {
+    x: 200,
+    y: 200,
+    radius: 50,
+    fill: 'red'
+  }
+
+  it('should add filter to the shape', () => {
+    const editor = createEditor()
+
+    const shape = editor.shapes.circle.insert(shapeConfig)
+
+    shape.addFilter({
+      name: 'Blur',
+      options: {
+        blurRadius: 20
+      }
+    })
+
+    expect(editor.board.getShapes()[0].node.attrs.filters.length).toBe(1)
+
+    expect(editor.board.getShapes()[0].node.attrs.filters[0]).toEqual(
+      Konva.Filters['Blur']
+    )
+  })
+
+  it('should add multiple filters to the shape', () => {
+    const editor = createEditor()
+
+    const shape = editor.shapes.circle.insert(shapeConfig)
+
+    shape.addFilter({
+      name: 'Blur',
+      options: {
+        blurRadius: 20
+      }
+    })
+
+    shape.addFilter({
+      name: 'Invert'
+    })
+
+    expect(editor.board.getShapes()[0].node.attrs.filters.length).toBe(2)
+
+    expect(editor.board.getShapes()[0].node.attrs.filters[0]).toEqual(
+      Konva.Filters['Blur']
+    )
+
+    expect(editor.board.getShapes()[0].node.attrs.filters[1]).toEqual(
+      Konva.Filters['Invert']
+    )
+  })
+
+  it('should remove filter from the shape', () => {
+    const editor = createEditor()
+
+    const shape = editor.shapes.circle.insert(shapeConfig)
+
+    shape.addFilter({
+      name: 'Blur',
+      options: {
+        blurRadius: 20
+      }
+    })
+
+    expect(editor.board.getShapes()[0].node.attrs.filters.length).toBe(1)
+
+    shape.removeFilter('Blur')
+
+    expect(editor.board.getShapes()[0].node.attrs.filters.length).toBe(0)
+  })
+
+  it('should remove all filters from the shape', () => {
+    const editor = createEditor()
+
+    const shape = editor.shapes.circle.insert(shapeConfig)
+
+    shape.addFilter({
+      name: 'Blur',
+      options: {
+        blurRadius: 20
+      }
+    })
+
+    shape.addFilter({
+      name: 'Invert'
+    })
+
+    expect(editor.board.getShapes()[0].node.attrs.filters.length).toBe(2)
+
+    shape.removeFilter('Blur')
+    shape.removeFilter('Invert')
+
+    expect(editor.board.getShapes()[0].node.attrs.filters.length).toBe(0)
+  })
+
+  it('should undo and redo after adding filter', () => {
+    console.warn = () => {}
+
+    const editor = createEditor()
+
+    const shape = editor.shapes.circle.insert(shapeConfig)
+
+    shape.addFilter({
+      name: 'Blur',
+      options: {
+        blurRadius: 20
+      }
+    })
+
+    expect(editor.board.getShapes()[0].node.attrs.filters.length).toBe(1)
+
+    editor.undo()
+    expect(editor.board.getShapes()[0].node.attrs.filters?.length).toBe(
+      undefined
+    )
+
+    editor.redo()
+    expect(editor.board.getShapes()[0].node.attrs.filters.length).toBe(1)
+  })
+
+  it('should undo and redo after removing filter', () => {
+    console.warn = () => {}
+
+    const editor = createEditor()
+
+    const shape = editor.shapes.circle.insert(shapeConfig)
+
+    shape.addFilter({
+      name: 'Blur',
+      options: {
+        blurRadius: 20
+      }
+    })
+
+    shape.removeFilter('Blur')
+    expect(editor.board.getShapes()[0].node.attrs.filters.length).toBe(0)
+
+    editor.undo()
+    expect(editor.board.getShapes()[0].node.attrs.filters?.length).toBe(1)
+
+    editor.redo()
+    expect(editor.board.getShapes()[0].node.attrs.filters.length).toBe(0)
+  })
+
+  it('should work with remove filter on shapes without filter', () => {
+    const editor = createEditor()
+
+    const shape = editor.shapes.circle.insert(shapeConfig)
+
+    expect(editor.board.getShapes()[0].node.attrs.filters?.length).toBe(
+      undefined
+    )
+
+    shape.removeFilter('Blur')
+
+    expect(editor.board.getShapes()[0].node.attrs.filters?.length).toBe(
+      undefined
+    )
+  })
+})
