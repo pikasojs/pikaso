4a2d9f959abd633c8c8c3d3332706413a4c2be81
Ramin <ramin.mousavy@gmail.com>
2020-12-21 01:04:41 +0300

    fix(coverage): ignore esm and cjs entry points

--- jest.config.ts
+++ jest.config.ts
@@ -20,15 +20,13 @@ export default {
   collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['src/**/*'],
+  collectCoverageFrom: ['src/**/*.ts'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ['src/index.cjs.ts', 'src/index.es.ts'],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
