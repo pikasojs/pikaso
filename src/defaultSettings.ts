d977087ee228b92fde790a6c4857ff3ba2bb99e6
Ramin <ramin.mousavy@gmail.com>
2021-09-19 20:05:50 +0300

    refactor: lint files

--- src/defaultSettings.ts
+++ src/defaultSettings.ts
@@ -13,65 +13,63 @@ const defaultTransformerSettings: Partial<Konva.TransformerConfig> = {
   anchorCornerRadius: 30
 }
 
-export const defaultSettings: Omit<
-  Settings,
-  'container' | 'width' | 'height'
-> = {
-  disableCanvasContextMenu: true,
-  containerClassName: 'pikaso',
-  transformer: defaultTransformerSettings,
-  cropper: {
+export const defaultSettings: Omit<Settings, 'container' | 'width' | 'height'> =
+  {
+    disableCanvasContextMenu: true,
+    containerClassName: 'pikaso',
     transformer: defaultTransformerSettings,
-    circular: false,
-    fixed: false,
-    keepRatio: true,
-    aspectRatio: 1,
-    minWidth: 100,
-    minHeight: 100,
-    marginRatio: 1.1,
-    overlay: {
-      color: '#262626',
-      opacity: 0.5
-    },
-    guides: {
-      show: true,
-      count: 3,
-      color: '#fff',
-      width: 1,
-      dash: [15, 10]
-    }
-  },
-  drawing: {
-    keyboard: {
-      cancelOnEscape: true
-    }
-  },
-  selection: {
-    interactive: true,
-    keyboard: {
-      enabled: true,
-      movingSpaces: 5,
-      map: {
-        delete: ['Backspace', 'Delete'],
-        moveLeft: ['ArrowLeft'],
-        moveRight: ['ArrowRight'],
-        moveUp: ['ArrowUp'],
-        moveDown: ['ArrowDown'],
-        deselect: ['Escape']
+    cropper: {
+      transformer: defaultTransformerSettings,
+      circular: false,
+      fixed: false,
+      keepRatio: true,
+      aspectRatio: 1,
+      minWidth: 100,
+      minHeight: 100,
+      marginRatio: 1.1,
+      overlay: {
+        color: '#262626',
+        opacity: 0.5
+      },
+      guides: {
+        show: true,
+        count: 3,
+        color: '#fff',
+        width: 1,
+        dash: [15, 10]
       }
     },
-    transformer: {
-      ...defaultTransformerSettings,
-      borderDash: [0, 0]
+    drawing: {
+      keyboard: {
+        cancelOnEscape: true
+      }
     },
-    zone: {
-      fill: 'rgba(105, 105, 105, 0.7)',
-      stroke: '#dbdbdb'
-    }
-  },
-  history: {
-    keyboard: {
-      enabled: true
+    selection: {
+      interactive: true,
+      keyboard: {
+        enabled: true,
+        movingSpaces: 5,
+        map: {
+          delete: ['Backspace', 'Delete'],
+          moveLeft: ['ArrowLeft'],
+          moveRight: ['ArrowRight'],
+          moveUp: ['ArrowUp'],
+          moveDown: ['ArrowDown'],
+          deselect: ['Escape']
+        }
+      },
+      transformer: {
+        ...defaultTransformerSettings,
+        borderDash: [0, 0]
+      },
+      zone: {
+        fill: 'rgba(105, 105, 105, 0.7)',
+        stroke: '#dbdbdb'
+      }
+    },
+    history: {
+      keyboard: {
+        enabled: true
+      }
     }
   }
-}
